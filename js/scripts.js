eb76a4be79fcd1f9853dd314a72048ceb13d775c
luisjg <luis.guzman@metalab.csun.edu>
2017-07-09 21:03:06 -0700

    Make the javascript less complicated.

--- js/scripts.js
+++ js/scripts.js
@@ -11,22 +11,22 @@ function checkVisible(element) {
 function makeActive(item) {
   label = '#' + $(item).attr('id') + '-label';
   $(label).css('fontWeight', 'bold');
-  $(item).removeClass('purple lighten-3 hoverable').addClass('purple hoverable');
+  $(item).removeClass('lighten-3');
 }
 
 function makeInactive(item) {
   label = '#' + $(item).attr('id') + '-label';
   $(label).css('fontWeight', '');
-  $(item).removeClass('purple hoverable').addClass('purple lighten-3 hoverable');
+  $(item).addClass('lighten-3');
 }
 
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   $('#current-year').text(moment().format('YYYY'));
-  
+
   // handle the button clicks
   var windowLocation = $(location).attr('pathname');
-  if(windowLocation.indexOf('presentations') < 0) {   
+  if(windowLocation.indexOf('presentations') < 0) {
     $('#work').click(function() {
       checkVisible($(this));
       $('#work-section').removeClass('inactive');
@@ -55,22 +55,22 @@ $(document).ready(function() {
       $('#projects-section').addClass('inactive');
       $('#skills-section').addClass('inactive');
     });
-    
+
     // paint the experience timeline
-    $('#experience-timeline').each(function() {          
+    $('#experience-timeline').each(function() {
       $this = $(this); // Store reference to this
       $userContent = $this.children('div'); // user content
-      
+
       // Create each timeline block
       $userContent.each(function() {
         $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
       });
-      
+
       // Add icons to each block
       $this.find('.vtimeline-point').each(function() {
         $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
       });
-      
+
       // Add dates to the timeline if exists
       $this.find('.vtimeline-content').each(function() {
         var date = $(this).data('date');
