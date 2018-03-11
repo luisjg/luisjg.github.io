ff36a1516d369965edec5ecee928dbe9f45ac216
luisjg <luis.guzman@metalab.csun.edu>
2018-03-11 14:20:45 -0700

    Add the Card component for future use.

---
+++ vue-redesign/src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="column" id="card">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="../assets/faculty.png" alt="Faculty Application image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="content">
+          <p class="has-text-centered title is-4"><a v-on:click.prevent="updateModal" href="#" :id="cardIndex" class="app-links" v-html="cardData.title"></a></p>
+          <span v-html="cardData.description"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from '@/components/Modal'
+export default {
+  methods: {
+    modalAction: function (event) {
+      document.documentElement.classList.add('is-clipped')
+      this.updateModal()
+    },
+    updateModal: function () {
+      this.$emit('currentModal', this.cardIndex)
+    }
+  },
+  components: {
+    'modal': Modal
+  },
+  props: ['cardIndex', 'cardData']
+}
+</script>
+
