74d2e27509f9eea37379c515258f22865079b33e
luisjg <luis.guzman@metalab.csun.edu>
2018-04-21 18:51:35 -0700

    Add underline style to text.

--- vue-redesign/src/components/Home.vue
+++ vue-redesign/src/components/Home.vue
@@ -2,13 +2,13 @@
   <div id="home" class="container">
     <h2 class="title">Hello there!</h2>
     <p class="is-size-4-desktop tab">
-      I'm a <strong>Web Developer</strong> from the
-      <strong>Los Angeles</strong> area. I currently work for
+      I'm a <strong class="fancy-underline">Web Developer</strong> from the
+      <strong class="fancy-underline">Los Angeles</strong> area. I currently work for
       <strong>
         CSUN
         <a href="https://www.metalab.csun.edu/" target="_blank">META<span class="red-text">+</span>LAB</a>
-      </strong> where we build <strong>job ready students</strong> and
-      <strong>web applications</strong>. Our mission is to <strong>mentor</strong>
+      </strong> where we build <strong class="fancy-underline">job ready students</strong> and
+      <strong class="fancy-underline">web applications</strong>. Our <span>mission</span> is to <strong class="fancy-underline">mentor</strong>
       the students by providing them with our
       <strong>software engineering</strong> tool-set which revolves around
       <strong>Laravel</strong>, <strong>Git</strong>, <strong>AWS</strong> and
@@ -23,5 +23,8 @@
  }
  .tab {
    text-indent:5%;
+  }
+  .fancy-underline {
+    text-decoration: underline dotted #d00d2d;
   }
 </style>
