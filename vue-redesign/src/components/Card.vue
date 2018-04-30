5b58be72171ceb8d27f5eba25bf1a1816bd54aa2
luisjg <luis.guzman@metalab.csun.edu>
2018-04-29 18:39:57 -0700

    Code is more refined.

--- vue-redesign/src/components/Card.vue
+++ vue-redesign/src/components/Card.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="{{class}}">
-    <div class="card">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="../assets/faculty.png" alt="Faculty Application image">
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <p class="has-text-centered title is-4"><a v-on:click.prevent="updateModal" href="#" :id="cardIndex" class="app-links" v-html="cardData.title"></a></p>
-          <span v-html="cardData.description"></span>
-        </div>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="'../assets/' + cardIndex + '.png'" :alt="cardData.title + 'Application image'">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="content">
+        <p class="has-text-centered title is-4"><a v-on:click.prevent="updateModal" href="#" :id="cardIndex" v-html="cardData.title"></a></p>
+        <span v-html="cardData.description"></span>
       </div>
     </div>
   </div>
@@ -31,7 +29,7 @@ export default {
   components: {
     'modal': Modal
   },
-  props: ['cardIndex', 'cardData', 'class']
+  props: ['cardIndex', 'cardData']
 }
 </script>
 
