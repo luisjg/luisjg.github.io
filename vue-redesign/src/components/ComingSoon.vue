3125b88706a3472ca7ece332471883c54898465f
luisjg <luis.guzman@metalab.csun.edu>
2018-08-19 13:06:00 -0700

    Include a placeholder component.

---
+++ vue-redesign/src/components/ComingSoon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="not-found" class="container has-text-centered">
+    <h2 class="title">Coming Soon!</h2>
+    <p class="is-size-4-desktop">
+      Hang on tight, this is not ready yet!<br>
+      <a @click="$router.go(-1)">Take me back to where I came from!</a>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+ #not-found {
+   padding: 3rem 1.5rem;
+ }
+</style>
+
+
