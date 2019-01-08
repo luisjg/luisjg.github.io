1d1756ecdba9d6cfcb2976876c04583ad7add016
luisjg <luis.guzman@metalab.csun.edu>
2019-01-07 21:49:45 -0800

    Remove extra class, add title and padding.

--- vue-redesign/src/components/Hobbies.vue
+++ vue-redesign/src/components/Hobbies.vue
@@ -11,25 +11,38 @@
       </div>
     </section>
 
-    <div class="section">
-      <div class="columns is-centered">
-        <div class="column is-one-fifth-desktop is-one-half-tablet">
-          <figure class="image">
-            <img src="../assets/pexels-photo-1905741.jpg" alt="cars">
-          </figure>
+    <div class="columns is-centered">
+      <div class="column is-one-fifth-desktop is-one-half-tablet">
+        <figure class="image">
+          <img src="../assets/pexels-photo-1905741.jpg" alt="cars">
+        </figure>
+        <div class="pt">
+          <p class="has-text-centered title is-4">Cars</p>
         </div>
-        <div class="column is-one-fifth-desktop is-one-half-tablet">
-          <figure class="image">
-            <img src="../assets/rtdiii-retro-a6000-nikon-76524.jpg" alt="photography">
-          </figure>
+      </div>
+      <div class="column is-one-fifth-desktop is-one-half-tablet">
+        <figure class="image">
+          <img src="../assets/rtdiii-retro-a6000-nikon-76524.jpg" alt="photography">
+        </figure>
+        <div class="pt">
+          <p class="has-text-centered title is-4">Photography</p>
         </div>
-        <div class="column is-one-fifth-desktop is-one-half-tablet">
-          <figure class="image">
-            <img src="../assets/pexels-photo-358666.jpg" alt="music">
-          </figure>
+      </div>
+      <div class="column is-one-fifth-desktop is-one-half-tablet">
+        <figure class="image">
+          <img src="../assets/pexels-photo-358666.jpg" alt="music">
+        </figure>
+        <div class="pt">
+          <p class="has-text-centered title is-4">Music</p>
         </div>
       </div>
     </div>
 
   </div>
 </template>
+
+<style scoped>
+  .pt {
+    padding-top: 1.5rem;
+  }
+</style>
