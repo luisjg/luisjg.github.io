6d3f122cddaa7ba07549f549d6f7e462b1fdf637
luisjg <luis.guzman@metalab.csun.edu>
2019-01-07 21:52:04 -0800

    Remove the extra class.

--- vue-redesign/src/components/Portfolio.vue
+++ vue-redesign/src/components/Portfolio.vue
@@ -11,39 +11,37 @@
       </div>
     </section>
 
-    <div class="section">
-      <div class="columns is-centered">
-        <!-- <div v-for="item in this.shortenedArray" :key="item" class="colum is-one-fifth-desktop is-one-half-tablet">
-          {{ item }}
-        </div> -->
-        <div class="column is-one-fifth-desktop is-one-half-tablet">
-          <figure class="image">
-            <img src="../assets/faculty.png" alt="cars">
-          </figure>
-          <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="faculty" href="#" class="app-links" v-html="workData.faculty.title"></a></p>
-          <div class="has-text-centered" v-html="workData.faculty.description"></div>
-        </div>
-        <div class="column is-one-fifth-desktop is-one-half-tablet">
-          <figure class="image">
-            <img src="../assets/scholarship.png" alt="photography">
-          </figure>
-          <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="scholarship" href="#" class="app-links" v-html="workData.scholarship.title"></a></p>
-          <div class="has-text-centered" v-html="workData.scholarship.description"></div>
-        </div>
-        <div class="column is-one-fifth-desktop is-one-half-tablet">
-          <figure class="image">
-            <img src="../assets/etd.png" alt="music">
-          </figure>
-          <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="etd" href="#" class="app-links" v-html="workData.etd.title"></a></p>
-          <div class="has-text-centered" v-html="workData.etd.description"></div>
-        </div>
-        <div class="column is-one-fifth-desktop is-one-half-tablet">
-          <figure class="image">
-            <img src="../assets/aa2.png" alt="music">
-          </figure>
-          <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="aa2" href="#" class="app-links" v-html="workData.aa2.title"></a></p>
-          <div class="has-text-centered" v-html="workData.aa2.description"></div>
-        </div>
+    <div class="columns is-centered">
+      <!-- <div v-for="item in this.shortenedArray" :key="item" class="colum is-one-fifth-desktop is-one-half-tablet">
+        {{ item }}
+      </div> -->
+      <div class="column is-one-fifth-desktop is-one-half-tablet">
+        <figure class="image">
+          <img src="../assets/faculty.png" alt="cars">
+        </figure>
+        <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="faculty" href="#" class="app-links" v-html="workData.faculty.title"></a></p>
+        <div class="has-text-centered" v-html="workData.faculty.description"></div>
+      </div>
+      <div class="column is-one-fifth-desktop is-one-half-tablet">
+        <figure class="image">
+          <img src="../assets/scholarship.png" alt="photography">
+        </figure>
+        <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="scholarship" href="#" class="app-links" v-html="workData.scholarship.title"></a></p>
+        <div class="has-text-centered" v-html="workData.scholarship.description"></div>
+      </div>
+      <div class="column is-one-fifth-desktop is-one-half-tablet">
+        <figure class="image">
+          <img src="../assets/etd.png" alt="music">
+        </figure>
+        <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="etd" href="#" class="app-links" v-html="workData.etd.title"></a></p>
+        <div class="has-text-centered" v-html="workData.etd.description"></div>
+      </div>
+      <div class="column is-one-fifth-desktop is-one-half-tablet">
+        <figure class="image">
+          <img src="../assets/aa2.png" alt="music">
+        </figure>
+        <p class="has-text-centered title is-4"><a @click.prevent="modalAction" id="aa2" href="#" class="app-links" v-html="workData.aa2.title"></a></p>
+        <div class="has-text-centered" v-html="workData.aa2.description"></div>
       </div>
     </div>
 
