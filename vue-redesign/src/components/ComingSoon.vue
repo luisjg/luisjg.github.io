c4cb28b745062c12a4de23fab69b30f31529bdd2
luisjg <luis.guzman@metalab.csun.edu>
2019-01-13 16:58:23 -0800

    Adjust padding.

--- vue-redesign/src/components/ComingSoon.vue
+++ vue-redesign/src/components/ComingSoon.vue
@@ -1,17 +1,11 @@
 <template>
-  <div id="not-found" class="container has-text-centered">
-    <h2 class="title">Coming Soon!</h2>
-    <p class="is-size-4-desktop">
-      Hang on tight, this is not ready yet!<br>
-      <a @click="$router.go(-1)">Take me back to where I came from!</a>
-    </p>
-  </div>
+  <section class="section">
+    <div class="container has-text-centered">
+      <h2 class="title">Coming Soon!</h2>
+      <p class="is-size-4-desktop">
+        Hang on tight, this is not ready yet!<br>
+        <a @click="$router.go(-1)">Take me back to where I came from!</a>
+      </p>
+    </div>
+  </section>
 </template>
-
-<style scoped>
- #not-found {
-   padding: 3rem 1.5rem;
- }
-</style>
-
-
--- vue-redesign/src/components/Home.vue
+++ vue-redesign/src/components/Home.vue
@@ -1,26 +1,25 @@
 <template>
-  <div id="home" class="container">
-    <h2 class="title">Hello there!</h2>
-    <p class="is-size-4-desktop tab">
-      I'm a <strong>Web Developer</strong> from the
-      <strong>Los Angeles</strong> area. I currently work for
-      <strong>
-        CSUN
-        <a href="https://www.metalab.csun.edu/" rel="noreferrer" target="_blank" aria-label="Find out more about META+LAB">META<span class="red-text">+</span>LAB</a>
-      </strong> where we build <strong>job ready students</strong> and
-      <strong>web applications</strong>. Our <span>mission</span> is to <strong>mentor</strong>
-      the students by providing them with our
-      <strong>software engineering</strong> tool-set which revolves around
-      <strong>Laravel</strong>, <strong>Git</strong>, <strong>AWS</strong> and
-      <strong>Agile Scrum</strong>.
-    </p>
-  </div>
+    <section class="section">
+      <div class="container">
+        <h2 class="title">Hello there!</h2>
+        <p class="is-size-4-desktop tab">
+          I'm a <strong>Web Developer</strong> from the
+          <strong>Los Angeles</strong> area. I currently work for
+          <strong>
+            CSUN
+            <a href="https://www.metalab.csun.edu/" rel="noreferrer" target="_blank" aria-label="Find out more about META+LAB">META<span class="red-text">+</span>LAB</a>
+          </strong> where we build <strong>job ready students</strong> and
+          <strong>web applications</strong>. Our <span>mission</span> is to <strong>mentor</strong>
+          the students by providing them with our
+          <strong>software engineering</strong> tool-set which revolves around
+          <strong>Laravel</strong>, <strong>Git</strong>, <strong>AWS</strong> and
+          <strong>Agile Scrum</strong>.
+        </p>
+      </div>
+    </section>
 </template>
 
 <style scoped>
- #home {
-   padding: 3rem 1.5rem 2.3rem 0;
- }
  .tab {
    text-indent:5%;
   }
