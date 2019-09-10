a81ca98a8cce78166797a33b71f06dfcf3e994dc
luisjg <luis.guzman@metalab.csun.edu>
2019-09-09 22:26:32 -0700

    More WIP.

--- vue-redesign/src/components/Experience.vue
+++ vue-redesign/src/components/Experience.vue
@@ -12,8 +12,24 @@
     </section>
 
     <div class="columns is-centered pb-3">
+      <div class="column is-two-thirds-desktop is-four-fifths-mobile is-size-4-desktop">
+        <h3 class="has-text-left has-text-weight-bold"><u>Web Developer</u></h3>
+        <div class="has-text-left">CSU Northridge, Northridge CA</div>
+        <div class="has-text-left">November 2015 - Present</div>
+        <ul>
+          <li>Work with clients to develop product backlogs.</li>
+          <li>Authored and maintained documentation.</li>
+          <li>Implement and adhrere to change management workflow.</li>
+          <li>Designed and developed REST APIs used accross the University.</li>
+          <li>In charge of testing accessibility, usability, and security.</li>
+          <li>Mentored over 40 junior developers.</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="columns is-centered pb-3">
       <div class="column is-half">
-        <div class="columns">
+        <div class="columns is-two-thirds">
           <div class="column">
             <figure class="logo">
               <img class="image" src="https://res.cloudinary.com/dfhliq7vp/image/upload/v1547608037/luisjg/IT_Vertical_186_Black.svg" width="55%" alt="CSUN Central IT logo">
@@ -25,10 +41,12 @@
               <p>at CSU, Northridge</p>
               <p class="has-text-left is-size-5">November 2015 - Present</p>
               <ul>
-                <li>Testing</li>
-                <li>Testing</li>
-                <li>Testing</li>
-                <li>Testing</li>
+                <li>Work with clients to develop product backlogs.</li>
+                <li>Authored and maintained documentation.</li>
+                <li>Implement and adhrere to change management workflow.</li>
+                <li>Designed and developed REST APIs used accross the University.</li>
+                <li>In charge of testing accessibility, usability, and security.</li>
+                <li>Mentored over 40 junior developers.</li>
               </ul>
             </div>
           </div>
