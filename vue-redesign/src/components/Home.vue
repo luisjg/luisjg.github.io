df2544c2421c7a40e4cd0b39ec0c21b2b6c6dee0
luisjg <contact@luisjg.io>
2019-09-12 21:44:16 -0700

    Update the tagline.

--- vue-redesign/src/components/Home.vue
+++ vue-redesign/src/components/Home.vue
@@ -10,15 +10,24 @@
       <div class="column is-two-thirds-desktop is-four-fifths-mobile">
         <p class="is-size-4 tab">
           I'm a <strong>Software Engineer</strong> from the
-          <strong>Los Angeles</strong> area. I currently work for
-          <strong>
-            <a href="https://www.csun.edu/it" rel="noreferrer" target="_blank" aria-label="Find out more about CSUN IT">CSUN IT</a>
-          </strong> where we create <strong>job ready students</strong> and
-          <strong>web applications</strong>. Our <span>mission</span> is to <strong>mentor</strong>
-          the students by providing them with our
-          <strong>software engineering</strong> tool-set which revolves around
-          <strong>Laravel</strong>, <strong>Git</strong>, <strong>AWS</strong>, and
-          <strong>Agile Scrum</strong>.
+          <strong>Los Angeles</strong> area with <strong>3 years</strong> of experience working
+          as a <strong>Full Stack Web Developer</strong> for
+          <strong><a href="https://www.csun.edu" rel="noreferrer" target="_blank"
+          aria-label="Find out more about California State University, Northridge">
+            California State University, Northridge
+          </a></strong>. I have a <strong>proven</strong> and
+          <strong>reproducible</strong> track record of designing and
+          developing <strong>modern web applications</strong> and
+          <strong>REST APIs</strong> that meet the clients needs.
+          I'm a natural <strong>leader</strong> and <strong>mentor</strong>
+          that understands software is built by a <strong>team</strong> of
+          unique individuals striving for one <strong>goal</strong>.
+        </p>
+        <p class="is-size-4 has-text-centered pt-1-half">Interested in more? Then let's get in
+          <strong><a href="mailto:contact@luisjg.io" rel="noreferrer"
+            target="_blank" aria-label="Send me an e-mail, let's talk.">
+            touch</a>
+          </strong>!
         </p>
       </div>
     </div>
