4afece30526f896e4520013cb423e892a5af12be
luisjg <contact@luisjg.io>
2019-10-16 21:49:28 -0700

    Re-write the skills portion.

--- vue-redesign/src/components/Skills.vue
+++ vue-redesign/src/components/Skills.vue
@@ -16,7 +16,8 @@
         <div class="columns is-centered">
           <div class="column is-half">
             <div class="has-text-centered">
-              <i class="fab fa-7x fa-laravel"></i>
+              <i class="fab fa-7x fa-laravel russian-green"></i>
+              <p class="title is-4 pt-1-half">Back End</p>
               <div>
                 Modern Web Applications, APIs
               </div>
@@ -24,7 +25,8 @@
           </div>
           <div class="column is-half">
             <div class="has-text-centered">
-              <i class="fab fa-7x fa-js-square"></i>
+              <i class="fab fa-7x fa-js-square russian-green"></i>
+              <p class="title is-4 pt-1-half">Front End</p>
               <div>
                 HTML, CSS, JQuery, Vue.js, Socket.io
               </div>
@@ -32,9 +34,10 @@
           </div>
           <div class="column is-half">
             <div class="has-text-centered">
-              <i class="fab fa-7x fa-jenkins"></i>
+              <i class="fas fa-7x fa-conveyor-belt-alt russian-green"></i>
+              <p class="title is-4 pt-1-half">CI/CD</p>
               <div>
-                Travis CI
+                Travis CI, GitHub Actions
               </div>
             </div>
           </div>
@@ -42,20 +45,22 @@
       </div>
     </div>
 
-    <div class="columns is-centered">
+    <div class="columns is-centered pt-1-half pb-3">
       <div class="column is-half">
         <div class="columns is-centered">
           <div class="column is-half">
             <div class="has-text-centered">
-              <i class="fas fa-7x fa-database"></i>
+              <i class="fas fa-7x fa-database russian-green"></i>
+              <p class="title is-4 pt-1-half">Database</p>
               <div>
-                MySQL, Reids
+                MongoDB, MySQL, Redis
               </div>
             </div>
           </div>
           <div class="column is-half">
             <div class="has-text-centered">
-              <i class="far fa-7x fa-code-branch"></i>
+              <i class="far fa-7x fa-code-branch russian-green"></i>
+              <p class="title is-4 pt-1-half">Version Control</p>
               <div>
                 Git, GitHub
               </div>
@@ -63,7 +68,8 @@
           </div>
           <div class="column is-half">
             <div class="has-text-centered">
-              <i class="fab fa-7x fa-docker"></i>
+              <i class="fad fa-7x fa-server russian-green"></i>
+              <p class="title is-4 pt-1-half">DevOps</p>
               <div>
                 Docker, Kubernetes
               </div>
