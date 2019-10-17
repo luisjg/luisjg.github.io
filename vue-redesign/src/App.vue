d8258ccd2ddc915c53ab869e29a9f6638c420114
luisjg <contact@luisjg.io>
2019-10-16 21:49:15 -0700

    Remove unused code.

--- vue-redesign/src/App.vue
+++ vue-redesign/src/App.vue
@@ -13,7 +13,6 @@
         </div>
         <div class="pt-1-half" style="letter-spacing: .2rem;">
           <a href="https://vuejs.org" rel="noreferrer" target="_blank" aria-label="Visit the Vue homepage to find out more">
-            <!-- <svg viewBox="0 0 128 128" :width="adjustSvgSize"> -->
             <svg arialabelledby="vue-logo" viewBox="0 0 128 128" role="img">
               <title id="vue-logo">Vue JS Logo</title>
               <path d="m-2.3125e-8 8.9337 49.854 0.1586 14.167 24.47 14.432-24.47 49.547-0.1577-63.834 110.14zm126.98 0.6374-24.36 0.0207-38.476 66.052-38.453-66.052-24.749-0.0194 63.211 107.89zm-25.149-0.008-22.745 0.16758l-15.053 24.647-14.817-24.647-22.794-0.1679 37.731 64.476zM25.997 9.3929l23.002 0.0087M25.997 9.3929l23.002 0.0087" fill="none"></path>
@@ -22,7 +21,6 @@
             </svg>
           </a>
           <a href="https://github.com" rel="noreferrer" target="_blank" aria-label="Visit the GitHub homepage to find out more">
-            <!-- <svg viewBox="0 0 128 128" :width="adjustSvgSize"> -->
             <svg arialabelledby="github-logo" viewBox="0 0 128 128" role="img">
               <title id="github-logo">GitHub Logo</title>
               <path fill-rule="evenodd" clip-rule="evenodd" d="M64 5.103c-33.347 0-60.388 27.035-60.388 60.388 0 26.682 17.303 49.317 41.297 57.303 3.017.56 4.125-1.31 4.125-2.905 0-1.44-.056-6.197-.082-11.243-16.8 3.653-20.345-7.125-20.345-7.125-2.747-6.98-6.705-8.836-6.705-8.836-5.48-3.748.413-3.67.413-3.67 6.063.425 9.257 6.223 9.257 6.223 5.386 9.23 14.127 6.562 17.573 5.02.542-3.903 2.107-6.568 3.834-8.076-13.413-1.525-27.514-6.704-27.514-29.843 0-6.593 2.36-11.98 6.223-16.21-.628-1.52-2.695-7.662.584-15.98 0 0 5.07-1.623 16.61 6.19C53.7 35 58.867 34.327 64 34.304c5.13.023 10.3.694 15.127 2.033 11.526-7.813 16.59-6.19 16.59-6.19 3.287 8.317 1.22 14.46.593 15.98 3.872 4.23 6.215 9.617 6.215 16.21 0 23.194-14.127 28.3-27.574 29.796 2.167 1.874 4.097 5.55 4.097 11.183 0 8.08-.07 14.583-.07 16.572 0 1.607 1.088 3.49 4.148 2.897 23.98-7.994 41.263-30.622 41.263-57.294C124.388 32.14 97.35 5.104 64 5.104z"></path>
@@ -128,14 +126,6 @@
       'footer-content': () => import('@/components/Footer'),
       'nav-bar': () => import('@/components/NavBar')
     },
-    computed: {
-      adjustSvgSize: function () {
-        if (this.getWidth() <= 480) {
-          return '10%'
-        }
-        return '4%'
-      }
-    },
     methods: {
       ...mapActions([
         'initializeButterCms',
@@ -154,15 +144,6 @@
             })
           })
         }
-      },
-      getWidth: function () {
-        return Math.max(
-          document.body.scrollWidth,
-          document.documentElement.scrollWidth,
-          document.body.offsetWidth,
-          document.documentElement.offsetWidth,
-          document.documentElement.clientWidth
-        )
       }
     }
   }
