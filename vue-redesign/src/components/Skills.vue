b083020f33d9f837da2b28474b117abb64d3f26d
luisjg <contact@luisjg.io>
2019-10-21 20:31:26 -0700

    Finish up the re-design of the skills portion.

--- vue-redesign/src/components/Skills.vue
+++ vue-redesign/src/components/Skills.vue
@@ -15,7 +15,7 @@
       <div class="column is-half">
         <div class="columns is-centered">
           <div class="column is-half">
-            <div class="has-text-centered">
+            <div class="has-text-centered padded--top">
               <i class="fab fa-7x fa-laravel russian-green"></i>
               <p class="title is-4 pt-1-half">Back End</p>
               <div>
@@ -24,7 +24,7 @@
             </div>
           </div>
           <div class="column is-half">
-            <div class="has-text-centered">
+            <div class="has-text-centered padded--top">
               <i class="fab fa-7x fa-js-square russian-green"></i>
               <p class="title is-4 pt-1-half">Front End</p>
               <div>
@@ -33,7 +33,7 @@
             </div>
           </div>
           <div class="column is-half">
-            <div class="has-text-centered">
+            <div class="has-text-centered padded--top">
               <i class="fas fa-7x fa-conveyor-belt-alt russian-green"></i>
               <p class="title is-4 pt-1-half">CI/CD</p>
               <div>
@@ -45,11 +45,11 @@
       </div>
     </div>
 
-    <div class="columns is-centered pt-1-half pb-3">
+    <div class="columns is-centered">
       <div class="column is-half">
-        <div class="columns is-centered">
+        <div class="columns is-centered pb-4">
           <div class="column is-half">
-            <div class="has-text-centered">
+            <div class="has-text-centered padded--top">
               <i class="fas fa-7x fa-database russian-green"></i>
               <p class="title is-4 pt-1-half">Database</p>
               <div>
@@ -58,7 +58,7 @@
             </div>
           </div>
           <div class="column is-half">
-            <div class="has-text-centered">
+            <div class="has-text-centered padded--top">
               <i class="far fa-7x fa-code-branch russian-green"></i>
               <p class="title is-4 pt-1-half">Version Control</p>
               <div>
@@ -67,7 +67,7 @@
             </div>
           </div>
           <div class="column is-half">
-            <div class="has-text-centered">
+            <div class="has-text-centered padded--top">
               <i class="fad fa-7x fa-server russian-green"></i>
               <p class="title is-4 pt-1-half">DevOps</p>
               <div>
@@ -83,59 +83,8 @@
 </template>
 
 <style scoped>
-  .progress.is-deep-lilac::-webkit-progress-value {
-    background-color: #9b64bc;
-  }
-  .progress.is-deep-lilac::-moz-progress-bar {
-    background-color: #9b64bc;
-  }
-  .progress.is-deep-lilac::-ms-fill {
-    background-color: #9b64bc;
-  }
-  .progress.is-mummys-tomb::-webkit-progress-value {
-    background-color: #808D8E;
-  }
-  .progress.is-mummys-tomb::-moz-progress-bar {
-    background-color: #808D8E;
-  }
-  .progress.is-mummys-tomb::-ms-fill {
-    background-color: #808D8E;
-  }
-  .progress.is-blue-bell::-webkit-progress-value {
-    background-color: #A3A5C3;
-  }
-  .progress.is-blue-bell::-moz-progress-bar {
-    background-color: #A3A5C3;
-  }
-  .progress.is-blue-bell::-ms-fill {
-    background-color: #A3A5C3;
-  }
-  .progress.is-old-lavender::-webkit-progress-value {
-    background-color: #766C7F;
-  }
-  .progress.is-old-lavender::-moz-progress-bar {
-    background-color: #766C7F;
-  }
-  .progress.is-old-lavender::-ms-fill {
-    background-color: #766C7F;
-  }
-  .progress.is-russian-green::-webkit-progress-value {
-    background-color: #6d9f71;
-  }
-  .progress.is-russian-green::-moz-progress-bar {
-    background-color: #6d9f71;
-  }
-  .progress.is-russian-green::-ms-fill {
-    background-color: #6d9f71;
-  }
-  .progress.is-pastel-blue::-webkit-progress-value {
-    background-color: #A9D2D5;
-  }
-  .progress.is-pastel-blue::-moz-progress-bar {
-    background-color: #A9D2D5;
-  }
-  .progress.is-pastel-blue::-ms-fill {
-    background-color: #A9D2D5;
+  .padded--top {
+    padding-top: 1.5rem;
   }
 </style>
 
