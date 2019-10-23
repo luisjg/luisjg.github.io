f29efc5c49f6c16dc2c670bc1aea5e997f6e473c
luisjg <contact@luisjg.io>
2019-10-22 18:51:21 -0700

    Re-write to dynamically print the values from vue store.

--- vue-redesign/src/components/Experience.vue
+++ vue-redesign/src/components/Experience.vue
@@ -17,18 +17,7 @@
           <h3 class="has-text-weight-bold">Web Developer at California State University, Northridge | Northridge CA</h3>
           <p>November 2015 - Present</p>
           <ul>
-            <li>Project lead and Scrum Master of successfully deployed web applications that met or exceeded client's expectations.</li>
-            <li>Gathered requirements and feedback from clients in weekly meetings and application demos to develop and refine product backlogs.</li>
-            <li>Lead designer and developer of eight REST APIs deployed in production environments with over 13 million successful requests used across multiple university web applications.</li>
-            <li>Designed and developed REST APIs used across multiple university web applications.</li>
-            <li>Developed on 11 production applications using LAMP, Laravel MVC framework, HTML, CSS, JavaScript, and MySQL for resource management.</li>
-            <li>Maintained and redesigned legacy code by applying best practices which improved overall application speed and user satisfaction.</li>
-            <li>Collaborated with the operations team to analyze and identify issues with the web applications, databases, and server configuration.</li>
-            <li>Used test-driven development to write clean, maintainable, and testable code on production-level applications and APIs.</li>
-            <li>Developed training procedures and materials for junior developers in the proper use of Git, Scrum, LAMP, MySQL, MVVM, and MVC frameworks.</li>
-            <li>Trained and supervised junior developers on web development and software engineering from initial design to final delivery.</li>
-            <li>Established and maintained documentation standards for applications and web APIs.</li>
-            <li>Implemented and enforced a change management workflow system for all the development teams and projects.</li>
+            <li v-for="(item, value) in experienceData" :key="value">{{ item }}</li>
           </ul>
         </div>
       </div>
