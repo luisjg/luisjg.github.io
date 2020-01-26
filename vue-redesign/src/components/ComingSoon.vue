14ecce331f5cf08a202a774711daf9b54c43405b
luisjg <contact@luisjg.io>
2020-01-26 11:16:55 -0800

    Adjust the font size.

--- vue-redesign/src/components/ComingSoon.vue
+++ vue-redesign/src/components/ComingSoon.vue
@@ -9,7 +9,7 @@
     </section>
 
     <div class="container has-text-centered pb-4">
-      <p class="is-size-4">
+      <p class="is-size-5">
         Hang on tight, this is not ready yet!<br>
         <a @click="$router.go(-1)">Take me back to where I came from!</a>
       </p>
--- vue-redesign/src/components/Home.vue
+++ vue-redesign/src/components/Home.vue
@@ -8,7 +8,7 @@
 
     <div class="columns is-centered is-mobile pb-4">
       <div class="column is-two-thirds-desktop is-four-fifths-mobile">
-        <p class="is-size-4 tab">
+        <p class="is-size-5 tab">
           I'm a <strong>Software Engineer</strong> from the
           <strong>Los Angeles</strong> area with <strong>three years</strong>
           of experience working as a <strong>Full Stack Web Developer</strong>
@@ -25,7 +25,7 @@
           unique <strong>individuals</strong> striving to solve one
           <strong>goal</strong>.
         </p>
-        <p class="is-size-4 has-text-centered pt-1-half">Interested in more?
+        <p class="is-size-5 has-text-centered pt-1-half">Interested in more?
           Send me an <strong><a href="mailto:contact@luisjg.io"
             rel="noreferrer" target="_blank"
             aria-label="Send me an e-mail, let's talk.">email</a></strong>
