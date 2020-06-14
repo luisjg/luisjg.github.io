d45dc2a8804c4cd37b7a30eb46a8d11409bde384
luisjg <contact@luisjg.io>
2020-06-13 21:23:45 -0700

    Adjust the wording on the last paragrah and make years of exp dynamic.

--- vue-redesign/src/components/Home.vue
+++ vue-redesign/src/components/Home.vue
@@ -10,7 +10,7 @@
       <div class="column is-two-thirds-desktop is-four-fifths-mobile">
         <p class="is-size-5 tab">
           I'm a <strong>Software Engineer</strong> from the
-          <strong>Los Angeles</strong> area with <strong>three years</strong>
+          <strong>Los Angeles</strong> area with <strong>{{ this.diff }} years</strong>
           of experience working as a <strong>Full Stack Web Developer</strong>
           who understands how to translate a <strong>client’s needs</strong>
           into a straightforward <strong>software design</strong> that follows
@@ -25,11 +25,11 @@
           unique <strong>individuals</strong> striving to solve one
           <strong>goal</strong>.
         </p>
-        <p class="is-size-5 has-text-centered pt-1-half">Interested in more?
-          Send me an <strong><a href="mailto:contact@luisjg.io"
+        <p class="is-size-5 has-text-centered pt-1-half"> If you would like to
+          know more about me or are interested in collaborating,
+          feel free and send me an <strong><a href="mailto:contact@luisjg.io"
             rel="noreferrer" target="_blank"
-            aria-label="Send me an e-mail, let's talk.">email</a></strong>
-            and let's get in touch!
+            aria-label="Send me an e-mail, let's talk."><u>email</u></a></strong>.
         </p>
       </div>
     </div>
@@ -41,3 +41,21 @@
    text-indent:5%;
   }
 </style>
+
+<script>
+import moment from 'moment'
+import converter from 'number-to-words'
+export default {
+  data () {
+    return {
+      now: moment(),
+      then: moment('2016-02-14'),
+      diff: String
+    }
+  },
+  created () {
+    this.diff = this.now.diff(this.then, 'years').toString()
+    this.diff = converter.toWords(this.diff)
+  }
+}
+</script>
